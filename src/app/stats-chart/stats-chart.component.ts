bde29d3a8702a3aeeea1492dafb76dfdd217c85a
Lee Fitchett <lfitchett1@gmail.com>
2018-03-06 17:58:18 -0600

    got chart working

--- src/app/stats-chart/stats-chart.component.ts
+++ src/app/stats-chart/stats-chart.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Chart } from 'angular-highcharts';
+import { Dice } from '../../lib/dice';
 
 @Component({
   selector: 'app-stats-chart',
@@ -7,25 +8,41 @@ import { Chart } from 'angular-highcharts';
   styleUrls: ['./stats-chart.component.css']
 })
 export class StatsChartComponent implements OnInit {
-  chart = new Chart({
-    chart: {
-      type: 'line'
-    },
-    title: {
-      text: 'Linechart'
-    },
-    credits: {
-      enabled: false
-    },
-    series: [{
-      name: 'Line 1',
-      data: [1, 2, 3]
-    }]
-  });
+  private _dice: Dice;
+  chart: Chart;
 
-  constructor() { }
+  @Input()
+  set dice(d: Dice) {
+    this._dice = d;
+    this.setChart();
+  }
+  get dice() { return this._dice }
+
+
+  public get points(): Array<[number, number]> {
+    return [...this._dice.values.entries()]
+  }
 
   ngOnInit() {
+    this.setChart();
+  }
+
+  private setChart() {
+    this.chart = new Chart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'Probability'
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        name: 'Line 1',
+        data: this.points,
+      }]
+    });
   }
 
 }
