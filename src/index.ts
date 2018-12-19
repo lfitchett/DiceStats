296370f5941458a32ace845aa206415569a095a8
Lee Fitchett <lfitchett1@gmail.com>
2018-12-19 17:55:01 -0600

    command line args

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import { Dice } from "./dice_utils/Dice";
+import * as minimist from "minimist";
 
-let test = Dice.Parse("1d6");
+let args = minimist(process.argv.slice(2), {
+    alias: {
+        r: 'roll',
+    }
+});
+let input = `${args["_"]}`;
 
-let arr = Array.from(test.roll(15));
-console.log(arr);
+console.log(`Dice: ${input}`);
+let dice = Dice.Parse(input);
+
+if (args['r']) {
+    console.log(`Result of ${args['r']} rolls: ${Array.from(dice.roll(args['r'])).toString()}`);
+} else {
+    console.log("Stats");
+    dice.print();
+    console.log();
+}
